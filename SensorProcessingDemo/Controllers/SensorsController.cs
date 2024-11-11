297fd054d18a4ff111f5cb102468a3ad0651b87e
annygumeniuk <anny.gumeniuk@gmail.com>
2024-11-12 00:45:03 +0200

    Added graph of data on the index page (need fixes)

---
+++ SensorProcessingDemo/Controllers/SensorsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensorProcessingDemo.Services;
+using SensorProcessingDemo.Models;
+
+namespace SensorProcessingDemo.Controllers
+{
+    public class SensorsController : Controller
+    {
+        private readonly Data _dataService;
+
+        public SensorsController(Data dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public IActionResult Index()
+        {
+            // Create a view model and assign sensor data to it
+            var viewModel = new SensorViewModel
+            {
+                Sensors = _dataService.sensors
+            };
+
+            // Optionally, set other properties, e.g., SensorType
+            viewModel.SensorType = "Temperature"; // Or another value based on the context
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ SensorProcessingDemo/Models/SensorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SensorProcessingDemo.Models
+{
+    public class SensorViewModel
+    {
+        public List<Sensor> Sensors { get; set; }
+        public string SensorType { get; set; }
+        public int TotalSensors => Sensors?.Count ?? 0;
+    }
+}

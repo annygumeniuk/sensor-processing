270c1f13e19e75b17694e06cef9a9b24ce992819
annygumeniuk <anny.gumeniuk@gmail.com>
2024-11-12 00:18:41 +0200

    Fixed ids

--- SensorProcessingDemo/Services/Data.cs
+++ SensorProcessingDemo/Services/Data.cs
@@ -10,10 +10,10 @@ namespace SensorProcessingDemo.Services
             new Sensor() { Id = 2, Name = "Temperature", Value = 18.5m, dateTime = DateTime.ParseExact("2024-11-12-10-30-20", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},                        
             
             new Sensor() { Id = 3, Name = "Humidity", Value = 35m, dateTime = DateTime.ParseExact("2024-11-12-08-30-45", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
-            new Sensor() { Id = 2, Name = "Humidity", Value = 55m, dateTime = DateTime.ParseExact("2024-11-12-10-30-20", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
+            new Sensor() { Id = 4, Name = "Humidity", Value = 55m, dateTime = DateTime.ParseExact("2024-11-12-10-30-20", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
 
-            new Sensor() { Id = 3, Name = "Lighting", Value = 500m, dateTime = DateTime.ParseExact("2024-11-12-08-30-45", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
-            new Sensor() { Id = 2, Name = "Lighting", Value = 400m, dateTime = DateTime.ParseExact("2024-11-12-10-30-20", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
+            new Sensor() { Id = 5, Name = "Lighting", Value = 500m, dateTime = DateTime.ParseExact("2024-11-12-08-30-45", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
+            new Sensor() { Id = 6, Name = "Lighting", Value = 400m, dateTime = DateTime.ParseExact("2024-11-12-10-30-20", "yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture)},
         };
     }
 }

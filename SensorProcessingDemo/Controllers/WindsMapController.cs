26e9bf684e656a9e84a386ffe7701c8e87cd790a
annygumeniuk <anny.gumeniuk@gmail.com>
2025-03-11 22:20:48 +0200

    Integrated api to display map of winds

---
+++ SensorProcessingDemo/Controllers/WindsMapController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SensorProcessingDemo.Controllers
+{
+    public class WindsMapController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public WindsMapController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var apiKey = _configuration["WindyAPI:Key"]; // Read from appsettings.json or secrets
+            ViewData["WindyAPIKey"] = apiKey;
+            return View();
+        }
+    }
+}

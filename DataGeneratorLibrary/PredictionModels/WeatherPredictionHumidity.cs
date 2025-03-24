9f488c30cc553febb563f6f9580692760bca360b
annygumeniuk <anny.gumeniuk@gmail.com>
2025-03-24 22:35:02 +0200

    Just removed unused usings

--- DataGeneratorLibrary/PredictionModels/WeatherPredictionHumidity.cs
+++ DataGeneratorLibrary/PredictionModels/WeatherPredictionHumidity.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.ML.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DataGeneratorLibrary
 {
     public class WeatherPredictionHumidity
--- DataGeneratorLibrary/WeatherModel.cs
+++ DataGeneratorLibrary/WeatherModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.ML;
-using Microsoft.ML.Data;
+﻿using Microsoft.ML;
 using DataGeneratorLibrary.PredictionModels;
 
 namespace DataGeneratorLibrary
